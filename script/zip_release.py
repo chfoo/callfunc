708bbcef9d7597363a37392ec9aa9e1cf859751f
Christopher Foo <chris.foo@gmail.com>
2019-06-19 17:45:13 -0400

    zip_release.py: Update hdll build

--- script/zip_release.py
+++ script/zip_release.py
@@ -45,7 +45,7 @@ def add_bin(zip_filename: str, project_dir: str):
 
     filenames = (
         'bin/libffi.txt',
-        'bin/macos/callfunc-0.1.0_4cf71cb/callfunc.hdll',
+        'bin/macos/callfunc-0.1.0_d8c42c9/callfunc.hdll',
         'bin/macos/libffi-3.3-rc0_80d0710/ffi.h',
         'bin/macos/libffi-3.3-rc0_80d0710/ffitarget.h',
         'bin/macos/libffi-3.3-rc0_80d0710/libffi.7.dylib',
@@ -54,7 +54,7 @@ def add_bin(zip_filename: str, project_dir: str):
         'bin/windows-x86-64/libffi-3.3-rc0-1_20f4c8a/libffi.dll',
         'bin/windows-x86-64/libffi-3.3-rc0-1_20f4c8a/libffi.lib',
         'bin/windows-x86-64/libffi-3.3-rc0-1_20f4c8a/libffi.pdb',
-        'bin/windows-x86/callfunc-0.1.0_c8a02b0/callfunc.hdll',
+        'bin/windows-x86/callfunc-0.1.0_d8c42c9/callfunc.hdll',
         'bin/windows-x86/libffi-3.3-rc0-1_20f4c8a/ffi.h',
         'bin/windows-x86/libffi-3.3-rc0-1_20f4c8a/ffitarget.h',
         'bin/windows-x86/libffi-3.3-rc0-1_20f4c8a/libffi.dll',
