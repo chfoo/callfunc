8b3655f7dfab7aaa053c08fa52a971fdfffb9368
Christopher Foo <chris.foo@gmail.com>
2019-08-17 16:48:37 -0400

    travis: Test against hashlink 1.10

--- script/travis_install_hashlink.sh
+++ script/travis_install_hashlink.sh
@@ -6,10 +6,10 @@ SCRIPT_DIR=$(dirname "$BASH_SOURCE")
 
 mkdir -p $SCRIPT_DIR/../out
 cd $SCRIPT_DIR/../out
-if [ ! -d "hashlink-1.9" ]; then
-    wget https://github.com/HaxeFoundation/hashlink/archive/1.9.tar.gz -O hashlink.tar.gz
+if [ ! -d "hashlink-1.10" ]; then
+    wget https://github.com/HaxeFoundation/hashlink/archive/1.10.tar.gz -O hashlink.tar.gz
 fi
 tar -xvf hashlink.tar.gz
-cd hashlink-1.9
+cd hashlink-1.10
 make
 sudo make install
