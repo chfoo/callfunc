bcd6a336a1aaf82490519b7328292d0419be4fa3
Christopher Foo <chris.foo@gmail.com>
2019-12-04 00:37:38 -0500

    travis: Bump to version 4.0.2

--- script/get_libffi.sh
+++ script/get_libffi.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname "$BASH_SOURCE")
 mkdir -p "$SCRIPT_DIR/../out"
 cd "$SCRIPT_DIR/../out"
 if [ ! -d "libffi" ]; then
-    git clone --depth 20 https://github.com/libffi/libffi.git libffi
+    git clone --depth 200 https://github.com/libffi/libffi.git libffi
 fi
 cd libffi
 git checkout 80d07104c33045ea34a4d5185600495dc7461a12
