77d811fa1ed56a01eae8554720ea9d1a23de14a2
Christopher Foo <chris.foo@gmail.com>
2019-12-18 22:57:41 -0500

    Fix

--- script/get_emsdk.sh
+++ script/get_emsdk.sh
@@ -6,12 +6,10 @@ SCRIPT_DIR="$PWD"/$(dirname "$BASH_SOURCE")
 mkdir -p "$SCRIPT_DIR/../out"
 cd "$SCRIPT_DIR/../out"
 
-if [ ! -d "emsdk" ]; then
-    git clone --depth 10 https://github.com/emscripten-core/emsdk emsdk
-fi
+git clone --depth 10 https://github.com/emscripten-core/emsdk emsdk
 
 cd emsdk
-git pull
+
 ./emsdk install latest
 ./emsdk activate latest
 
