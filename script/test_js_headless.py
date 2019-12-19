da3eefb247a4b841495acf5d42332ee2269b17e2
Christopher Foo <chris.foo@gmail.com>
2019-12-18 23:58:26 -0500

    Fix

--- script/test_js_headless.py
+++ script/test_js_headless.py
@@ -2,20 +2,21 @@ import os.path
 import subprocess
 import sys
 import os
+import atexit
 import signal
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def main():
-    print('Starting server...')
+    print('Starting server...', file=sys.stderr)
 
     script_dir = os.path.dirname(__file__)
     root_dir = os.path.join(script_dir, '..')
     html_path = os.path.join(root_dir, 'test.html')
-    server_process = subprocess.Popen(['emrun', '--no_browser', '--no_emrun_detect', html_path])
+    server_process = subprocess.Popen(['emrun', '--no_browser', '--no_emrun_detect', html_path], preexec_fn=os.setsid)
 
-    print('Starting Chrome...')
+    print('Starting Chrome...', file=sys.stderr)
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -24,27 +25,38 @@ def main():
     browser = webdriver.Chrome(options=chrome_options, executable_path=driver_path)
     test_result = False
 
-    try:
-        print('Loading page...')
-        browser.get('http://localhost:6931/test.html')
-        print('Loaded.')
-
-        header_element = browser.find_element_by_css_selector(".header")
-        print(header_element.text)
-
-        summary_element = browser.find_element_by_css_selector(".headerinfo")
-        print(summary_element.text)
-
-        if header_element.text == 'TEST OK':
-            test_result = True
-
-    finally:
+    def cleanup_server():
         os.killpg(os.getpgid(server_process.pid), signal.SIGTERM)
         server_process.wait()
+
+    def cleanup_browser():
         browser.quit()
 
-        if not test_result:
-            sys.exit(1)
+    atexit.register(cleanup_server)
+    atexit.register(cleanup_browser)
+
+    print('Loading page...', file=sys.stderr)
+    browser.get('http://localhost:6931/test.html')
+    print('Loaded.', file=sys.stderr)
+
+    header_element = browser.find_element_by_css_selector(".header")
+    print(header_element.text, file=sys.stderr)
+
+    summary_element = browser.find_element_by_css_selector(".headerinfo")
+    print(summary_element.text, file=sys.stderr)
+
+    if header_element.text == 'TEST OK':
+        test_result = True
+
+    print('Cleanup', file=sys.stderr)
+    cleanup_browser()
+    cleanup_server()
+
+    print('Done', file=sys.stderr)
+
+    if not test_result:
+        sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
